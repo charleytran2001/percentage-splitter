9d76e0ed0ed929195309e7ea2b45b9aef671076d
charleytran2001 <44280830+charleytran2001@users.noreply.github.com>
2022-04-21 19:25:16 -0700

    fixed mouseup
    Bar stops moving when mouse is released. Bar also caps at 1%, but doesn't go past 99% for some reason.

--- script.js
+++ script.js
@@ -3,24 +3,40 @@ let bar1 = document.getElementsByClassName('bar')[0];
 let bar2 = document.getElementsByClassName('bar')[1];
 let resize1 = document.getElementsByClassName('resize')[0];
 
-// onmousemove = function (e) { console.log("mouse location:", e.clientX, e.clientY) }
-
 let mousePosition = 0;
 
-resize1.addEventListener("mousedown", function (e) {
+resize1.addEventListener('mousedown', function(e) {
     e.preventDefault();
-    barContainer.onmousemove = function (e) {
-        mousePosition = (e.pageX / barContainer.getBoundingClientRect().width) * 100;
-        console.log("mouse location:", mousePosition);
+    document.onmousemove = function(e) {
+        mousePosition = ((e.clientX - barContainer.getBoundingClientRect().left) / barContainer.getBoundingClientRect().width) * 100;
+        // console.log("mouse location:", mousePosition);
+        
+        switch(true) {
+            case (mousePosition <= 1):
+                console.log("a");
+                bar1.style.width = "1%";
+                bar2.style.width = "99%";
+                break;
+            case (mousePosition > 1 && mousePosition < 99):
+                console.log("b");
+                bar1.style.width = mousePosition + "%";
+                bar1.getElementsByTagName('p')[0].innerHTML = mousePosition.toFixed(2) + "%";
 
-        bar1.style.width = mousePosition + "%";
-        bar2.style.width = 100 - mousePosition + "%";
+                bar2.style.width = 100 - mousePosition + "%";
+                bar2.getElementsByTagName('p')[0].innerHTML = (100 - mousePosition).toFixed(2) + "%";
+                break;
+            case ((100 - mousePosition) >= 99):
+                console.log("c");
+                console.log("test");
+                bar1.style.width = "99%";
+                bar2.style.width = "1%";
+                break;
+        }
 
-        bar1.getElementsByTagName('p')[0].innerHTML = mousePosition.toFixed(2) + "%";
-        bar2.getElementsByTagName('p')[0].innerHTML = (100 - mousePosition).toFixed(2) + "%";
+        
     }
 });
 
-resize1.addEventListener("mouseup", function (e) {
-    resize1.onmousemove = null
+window.addEventListener('mouseup', function(e) {
+    document.onmousemove = null;
 });
